131ec7d9ae8f7671f1b2da4b6c6f304676619ad7
Desmond Lai <desmondlai30@gmail.com>
2021-09-10 01:16:06 -0400

    Implemented Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,29 @@
 export default function Footer() {
-    return(
-        <div>
+    return (
+        <footer className="mt-5 mb-5">
+            <div className="container">
+                <div className="footer-icons">
+                    <a href="https://www.dor.org/"><img src="/images/footer/1.png"
+                                                        alt="Roman Catholic Diocese of Rochester"/></a>
+                    <a href="https://rochester.edu/"><img src="/images/footer/2.png" alt="University of Rochester"/></a>
+                    <a href="https://www.rochester.edu/chapel/"><img
+                        src="/images/footer/3.png" alt="University of Rochester Interfaith Chapel"/></a>
+                    <a href="https://www.usccb.org/"><img
+                        src="/images/footer/4.png" alt="United States Conference of Catholic Bishops"/></a>
+                    <a href="https://www.vatican.va/content/vatican/en.html"><img
+                        src="/images/footer/5.png" alt="The Holy See"/></a>
+                </div>
+            </div>
 
-        </div>
+            <div className="small-print">
+                <div className="container">
+                    <p>© {new Date().getFullYear()} Catholic Newman Community at the University of Rochester | <a
+                        href="tel:585-275-8515">+1 (585)
+                        275-8515</a> | <a
+                        href="mailto:catholicnewmancommunity@rochester.edu">catholicnewmancommunity@rochester.edu</a>
+                    </p>
+                </div>
+            </div>
+        </footer>
     )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 
-export default function Building() {
+export default function About() {
     return(
         <div>
             <Head>
-                <title>UR Newman - Streaming</title>
+                <title>UR Newman - About</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <meta name="description" content="Catholic Community at the University of Rochester"/>
                 <link rel="icon" href="/favicon.ico"/>
