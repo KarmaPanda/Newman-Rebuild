07ee5e1a39fa7b650930910e78c435c982a11156
Desmond Lai <desmondlai30@gmail.com>
2021-09-10 01:29:10 -0400

    Added sub-pages

--- pages/yespa.js
+++ pages/yespa.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 
-export default function Streaming() {
+export default function YesPa() {
     return(
         <div>
             <Head>
-                <title>UR Newman - Streaming</title>
+                <title>UR Newman - Yes Pa</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <meta name="description" content="Catholic Community at the University of Rochester"/>
                 <link rel="icon" href="/favicon.ico"/>
