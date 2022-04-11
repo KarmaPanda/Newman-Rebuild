3d4e490aa5fd4cd3fbaa87e2edb4ecb54889d53b
Desmond Lai <desmondlai30@gmail.com>
2022-04-11 14:34:32 -0400

    Updated homepage banners.

--- components/HeroButttons.js
+++ components/HeroButttons.js
@@ -4,22 +4,22 @@ export default function HeroButttons() {
     return (
         <div className="container-fluid">
             <div className="row hero-buttons">
-                <div className="col-md-4 hero-buttons-item" style={{
+                <Link href="/about" passHref><div className="col-md-4 hero-buttons-item" style={{
                     backgroundImage: "url('../images/index/hero-buttons/1.jpg')"
                 }}>
-                    <h3><Link href="/about" passHref>Register/Update Information</Link></h3>
-                </div>
-                <div className="col-md-4 hero-buttons-item" style={{
+                    <h3>Register/Update Information</h3>
+                </div></Link>
+                <Link href="/giving" passHref><div className="col-md-4 hero-buttons-item" style={{
                     backgroundImage: "url('../images/index/hero-buttons/2.jpg')"
                 }}>
-                    <h3><Link href="/giving" passHref>Support Newman</Link></h3>
-                </div>
-                <div className="col-md-4 hero-buttons-item" style={{
+                    <h3>Support Newman</h3>
+                </div></Link>
+                <Link href="/groups/staff" passHref><div className="col-md-4 hero-buttons-item" style={{
                     backgroundImage: "url('../images/index/hero-buttons/3.jpg')",
                     backgroundPosition: "50% 20%"
                 }}>
-                    <h3><Link href="/groups/staff" passHref>Meet our Staff</Link></h3>
-                </div>
+                    <h3>Meet our staff</h3>
+                </div></Link>
             </div>
         </div>
     )
--- components/HeroCarousel.js
+++ components/HeroCarousel.js
@@ -10,89 +10,68 @@ export default function HeroCarousel() {
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="2" aria-label="Slide 3" />
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="3" aria-label="Slide 4" />
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="4" aria-label="Slide 5" />
+                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="5" aria-label="Slide 6" />
+                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="6" aria-label="Slide 7" />
+                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="7" aria-label="Slide 8" />
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/5.png" />
-                        <img width="100%" src="/images/index/5.png" alt="Spring 2021 Newman" />
-                    </picture>
-                </div>
-                <div className="carousel-item">
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/6.png" />
-                        <img width="100%" src="/images/index/6.png" alt="Sunday Mass" />
-                    </picture>
-                </div>
-                <div className="carousel-item">
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/7.png" />
-                        <img width="100%" src="/images/index/7.png" alt="InstaMass" />
-                    </picture>
-                </div>
-                <div className="carousel-item">
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/8.png" />
-                        <img width="100%" src="/images/index/8.png" alt="YesPa" />
-                    </picture>
-                </div>
-                {/*<div className="carousel-item active">
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/5.png" />
-                        <img width="100%" src="/images/index/5.png" alt="Spring 2021 Newman" />
-                    </picture>
-
-                    
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/1.png"/>
-                        <img width="100%" src="/images/index/1-mobile.png" alt="Summer 2021 Newman"/>
-                    </picture>
-                    <div className="container">
-                        <div className="carousel-caption">
-                            <button type="button" className="btn btn-primary btn-lg px-2 me-md-2">Register / Update
-                                Information
-                            </button>
-                            <button type="button" className="btn btn-success btn-lg px-4">Support Newman</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <picture>
-                        <source media="(min-width:1000px)" srcSet="/images/index/2.png" />
-                        <img width="100%" src="/images/index/2-mobile.png" alt="Sunday Mass at 10:30AM, InstaPrayer at Wednesday 9PM" />
-                    </picture>
-
-                    <div className="carousel-caption">
-                        <Link href="/streaming">
-                            <button type="button" className="btn btn-primary btn-lg px-2 me-md-2">Sunday Mass
-                            </button>
-                        </Link>
-                        <a href="https://rochester.zoom.us/j/2771228739">
-                            <button type="button" className="btn btn-success btn-lg px-4">InstaPrayer</button>
-                        </a>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <a href="/giving/annual-appeal">
+                    <Link href="/streaming">
                         <picture>
-                            <source media="(min-width:1000px)" srcSet="/images/index/3.png" />
-                            <img width="100%" src="/images/index/3-mobile.png" alt="Donate to our ministry here" />
+                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/1.png" />
+                            <img width="100%" src="/images/index/4112022/1.png" alt="Spring 2021 Newman" />
                         </picture>
-                    </a>
-
-                    <div className="carousel-caption">
-                        <Link href="/giving/annual-appeal">
-                            <button type="button" className="btn btn-success btn-lg px-2 me-md-2">
-                                Click here to give!
-                            </button>
-                        </Link>
-                    </div>
-                </div>*/}
+                    </Link>
+                </div>
+                <div className="carousel-item">
+                    <picture>
+                        <source media="(min-width:1000px)" srcSet="/images/index/4112022/2.png" />
+                        <img width="100%" src="/images/index/4112022/2.png" alt="Happy Easter" />
+                    </picture>
+                </div>
+                <div className="carousel-item">
+                    <Link href="/streaming">
+                        <picture>
+                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/3.png" />
+                            <img width="100%" src="/images/index/4112022/3.png" alt="Holy Week Schedule" />
+                        </picture>
+                    </Link>
+                </div>
+                <div className="carousel-item">
+                    <Link href="/giving/student-appeal">
+                        <picture>
+                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/4.png" />
+                            <img width="100%" src="/images/index/4112022/4.png" alt="Newman Student Appeal" />
+                        </picture>
+                    </Link>
+                </div>
+                <div className="carousel-item">
+                    <picture>
+                        <source media="(min-width:1000px)" srcSet="/images/index/4112022/5.png" />
+                        <img width="100%" src="/images/index/4112022/5.png" alt="Daily Mass" />
+                    </picture>
+                </div>
+                <div className="carousel-item">
+                    <Link href="/streaming">
+                        <picture>
+                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/6.png" />
+                            <img width="100%" src="/images/index/4112022/6.png" alt="Lent at Newman" />
+                        </picture>
+                    </Link>
+                </div>
+                <div className="carousel-item">
+                    <Link href="/streaming">
+                        <picture>
+                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/7.png" />
+                            <img width="100%" src="/images/index/4112022/7.png" alt="Sunday Mass" />
+                        </picture>
+                    </Link>
+                </div>
                 <div className="carousel-item">
                     <Link href="/building" passHref>
                         <picture>
-                            <source media="(min-width:1000px)" srcSet="/images/index/4.png" />
-                            <img width="100%" src="/images/index/4-mobile.png" alt="Click to learn about our building project" />
+                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/8.png" />
+                            <img width="100%" src="/images/index/4112022/8.png" alt="Click to learn about our building project" />
                         </picture>
                     </Link>
                 </div>
