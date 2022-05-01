9e77558c7d6bbb6e68ea5fffef9bb6a8fc7feac2
Desmond Lai <desmondlai30@gmail.com>
2022-05-01 00:27:45 -0400

    Removed banners.

--- components/HeroCarousel.js
+++ components/HeroCarousel.js
@@ -8,10 +8,6 @@ export default function HeroCarousel() {
                     aria-current="true" aria-label="Slide 1" />
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="1" aria-label="Slide 2" />
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="2" aria-label="Slide 3" />
-                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="3" aria-label="Slide 4" />
-                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="4" aria-label="Slide 5" />
-                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="5" aria-label="Slide 6" />
-                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="6" aria-label="Slide 7" />
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
@@ -21,42 +17,10 @@ export default function HeroCarousel() {
                     </picture>
                 </div>
                 <div className="carousel-item">
-                    <Link href="/streaming" passHref>
-                        <picture>
-                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/3.png" />
-                            <img width="100%" src="/images/index/4112022/3.png" alt="Holy Week Schedule" />
-                        </picture>
-                    </Link>
-                </div>
-                <div className="carousel-item">
                     <picture>
                         <source media="(min-width:1000px)" srcSet="/images/index/4302022/1.png" />
                         <img width="100%" src="/images/index/4302022/1.png" alt="Happy Easter" />
                     </picture>
-                </div>
-                <div className="carousel-item">
-                    <Link href="/giving/student-appeal" passHref>
-                        <picture>
-                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/4.png" />
-                            <img width="100%" src="/images/index/4112022/4.png" alt="Newman Student Appeal" />
-                        </picture>
-                    </Link>
-                </div>
-                <div className="carousel-item">
-                    <Link href="/streaming" passHref>
-                        <picture>
-                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/5.png" />
-                            <img width="100%" src="/images/index/4112022/5.png" alt="Daily Mass" />
-                        </picture>
-                    </Link>
-                </div>
-                <div className="carousel-item">
-                    <Link href="/streaming" passHref>
-                        <picture>
-                            <source media="(min-width:1000px)" srcSet="/images/index/4112022/7.png" />
-                            <img width="100%" src="/images/index/4112022/7.png" alt="Sunday Mass" />
-                        </picture>
-                    </Link>
                 </div>
                 <div className="carousel-item">
                     <Link href="/building" passHref>
