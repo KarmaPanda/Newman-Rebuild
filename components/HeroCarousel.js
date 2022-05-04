a69b778b604a9fc8fe4701ec20a5476f41cacaab
Desmond Lai <desmondlai30@gmail.com>
2022-05-04 16:14:56 -0400

    Added DOG 2022 banner.

--- components/HeroCarousel.js
+++ components/HeroCarousel.js
@@ -8,6 +8,7 @@ export default function HeroCarousel() {
                     aria-current="true" aria-label="Slide 1" />
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="1" aria-label="Slide 2" />
                 <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="2" aria-label="Slide 3" />
+                <button type="button" data-bs-target="#HeroCarousel" data-bs-slide-to="3" aria-label="Slide 4" />
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
@@ -17,9 +18,17 @@ export default function HeroCarousel() {
                     </picture>
                 </div>
                 <div className="carousel-item">
+                    <Link href="https://give.communityfunded.com/o/university-of-rochester/i/day-of-giving-2022/s/catholic-newman-community" passHref>
+                        <picture>
+                            <source media="(min-width:1000px)" srcSet="/images/index/542022/1.png" />
+                            <img width="100%" src="/images/index/542022/1.png" alt="Day of Giving 2022" />
+                        </picture>
+                    </Link>
+                </div>
+                <div className="carousel-item">
                     <picture>
                         <source media="(min-width:1000px)" srcSet="/images/index/4302022/1.png" />
-                        <img width="100%" src="/images/index/4302022/1.png" alt="Happy Easter" />
+                        <img width="100%" src="/images/index/4302022/1.png" alt="Baccalaureate Mass" />
                     </picture>
                 </div>
                 <div className="carousel-item">
