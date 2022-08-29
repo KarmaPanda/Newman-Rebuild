2556c642f9adb3e2772b3aa38a5310ae3c7f3c44
Desmond Lai <desmondlai30@gmail.com>
2022-08-29 16:38:27 -0400

    Fixed error with previous commit and removed 404 redirect.

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,15 @@
 import Error from 'next/error'
-import redirect from 'nextjs-redirect'
+//import redirect from 'nextjs-redirect'
 
 import { useRouter } from 'next/router'
 import Notification from '../components/Notification'
 
 export default function Page() {
     const router = useRouter()
-    const Redirect = redirect("https://urnewman-legacy.herokuapp.com" + router.asPath)
+    //const Redirect = redirect("https://urnewman-legacy.herokuapp.com" + router.asPath)
 
-    return <Redirect>
-        <Notification message={"If you see this 404 page, you are being redirected to our old website. Please wait..."}/>
+    return <>
+        {/*<Notification message={"If you see this 404 page, you are being redirected to our old website. Please wait..."} />*/}
         <Error statusCode={404} />
-    </Redirect>
+    </>
 }
