294cd71986c31e2a60e8d8b4bc3b433e716fa9a0
KarmaPanda <desmondlai30@gmail.com>
2023-02-24 18:24:58 -0500

    Fixed streaming website errors.

--- db/db.js
+++ db/db.js
@@ -1,8 +1,26 @@
 const { Sequelize } = require("sequelize");
 
-let sequelize
+let sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: process.env.NODE_ENV === "development",
+  }
+);
 
-if (process.env.NODE_ENV !== 'development') {
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
+/*if (process.env.NODE_ENV !== 'development') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         logging: false,
         dialectOptions: {
@@ -12,9 +30,9 @@ if (process.env.NODE_ENV !== 'development') {
         }
     })
 } else {
-    if (process.env.DB_CONNECTIONSTRING !== null) {
+    if (process.env.DB_CONNECTIONSTRING !== undefined) {
+        console.log("Using DB_CONNECTIONSTRING")
         sequelize = new Sequelize(process.env.DB_CONNECTIONSTRING, {
-            logging: false,
             dialectOptions: {
                 ssl: {
                     rejectUnauthorized: false
@@ -22,10 +40,10 @@ if (process.env.NODE_ENV !== 'development') {
             }
         })
     } else {
+        console.log("USING DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST")
         sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
             host: process.env.DB_HOST,
             dialect: 'postgres',
-            logging: false,
             dialectOptions: {
                 ssl: {
                     rejectUnauthorized: false
@@ -33,6 +51,6 @@ if (process.env.NODE_ENV !== 'development') {
             }
         });
     }
-}
+}*/
 
-module.exports = { sequelize }
+module.exports = { sequelize };
--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,94 +1,117 @@
-const cron = require('node-cron');
-const axios = require('axios');
+const cron = require("node-cron");
+const axios = require("axios");
 const Streaming = require("./models/Streaming");
-const { sequelize } = require('./db');
+const { sequelize } = require("./db");
 
 async function syncClient() {
-    await sequelize.sync({ alter: true }).catch(e => {
-        console.error("[SYNC-CLIENT]: Cannot connect to database.")
-        console.error("[SYNC-CLIENT]:", e.message)
-    })
+  await sequelize.sync({ alter: true }).catch((e) => {
+    console.error("[SYNC-CLIENT]: Cannot connect to database.");
+    console.error("[SYNC-CLIENT]:", e.message);
+  });
 
-    console.log("[SYNC-CLIENT]: Successfully synced tables!")
+  console.log("[SYNC-CLIENT]: Successfully synced tables!");
 }
 
 async function getYoutubeStreamFromDatabase() {
-    let data = await Streaming.findAll({
-        limit: 1,
-        order: [['createdAt', 'DESC']]
-    }).catch(e => {
-        console.error("[GET-YT-STREAM] Query Error: " + e.message)
-        return null
-    });
+  syncClient();
+  let data = await Streaming.findAll({
+    limit: 1,
+    order: [["createdAt", "DESC"]],
+  }).catch((e) => {
+    console.error("[GET-YT-STREAM] Query Error: " + e.message);
+    return null;
+  });
 
-    if (data.length > 0) {
-        return data[0].livestream_id
-    } else {
-        return null
-    }
+  if (data.length > 0) {
+    return data[0].livestream_id;
+  } else {
+    updateVideoIdFromAPI();
+    return null;
+  }
 }
 
 async function updateYoutubeStream(videoId) {
-    syncClient().then(_ => {
-        Streaming.count({
-            where: {
-                livestream_id: videoId
-            }
-        }).then(res => {
-            console.log("[Update-YT-Stream]: Is Unique:", res <= 1)
-            if (res <= 1) {
-                Streaming.create({
-                    id: null,
-                    livestream_id: videoId
-                }).then(res => {
-                    console.log("[Update-YT-Stream] Updated livestream id to:", videoId)
-                    return videoId
-                }).catch(e => {
-                    console.error("[Update-YT-Stream] Query Error: " + e.message)
-                    return null
-                })
-            }
-        }).catch(e => {
-            console.error("[Update-YT-Stream] Query Error: " + e.message)
-            return null
+  syncClient()
+    .then((_) => {
+      Streaming.count({
+        where: {
+          livestream_id: videoId,
+        },
+      })
+        .then((res) => {
+          console.log("[Update-YT-Stream]: Is Unique:", res <= 1);
+          if (res <= 1) {
+            Streaming.create({
+              id: null,
+              livestream_id: videoId,
+            })
+              .then((res) => {
+                console.log(
+                  "[Update-YT-Stream] Updated livestream id to:",
+                  videoId
+                );
+                return videoId;
+              })
+              .catch((e) => {
+                console.error("[Update-YT-Stream] Query Error: " + e.message);
+                return null;
+              });
+          }
         })
-    }).catch(e => {
-        console.error("[Update-YT-Stream] Sync Error: " + e.message)
-        return null
+        .catch((e) => {
+          console.error("[Update-YT-Stream] Query Error: " + e.message);
+          return null;
+        });
     })
-}
-
-async function updateVideoIdFromAPI() {
-    let videoId = await getYoutubeStreamFromDatabase()
-    let channelId = "UCYRxyoQ0THt7m3pruEKNw3A";
-    let apiKey = process.env.YT_API_KEY;
-    let queryString = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + channelId + "&eventType=live&type=video&key=" + apiKey;
-
-    axios.get(queryString).then(response => {
-        if (response.status.valueOf() !== 403) {
-            console.log("[YT-API]: Received the following from Youtube API: ")
-            console.log(response.data)
-
-            let youtube = response.data;
-
-            if (youtube.items !== null) {
-                if (youtube.items.length > 0) {
-                    videoId = youtube.items[0].id.videoId;
-                    console.log("[YT-API]: New videoId: " + videoId)
-                    updateYoutubeStream(videoId)
-                } else {
-                    console.error("[YT-API]: No new items to update.")
-                    updateYoutubeStream("XdMBqIeeADA")
-                }
-            } else {
-                console.error("[YT-API]: Error reading items from response data.")
-            }
-        } else {
-            console.error("[YT-API]: Quota reached for Youtube API.")
-        }
-    }).catch(e => {
-        console.error("[YT-API]: API Refresh Error: " + e.message);
+    .catch((e) => {
+      console.error("[Update-YT-Stream] Sync Error: " + e.message);
+      return null;
     });
 }
 
-module.exports = { syncClient, getYoutubeStreamFromDatabase, updateYoutubeStream, updateVideoIdFromAPI }
+async function updateVideoIdFromAPI() {
+  let videoId = await getYoutubeStreamFromDatabase();
+  let channelId = "UCYRxyoQ0THt7m3pruEKNw3A";
+  let apiKey = process.env.YT_API_KEY;
+  let queryString =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" +
+    channelId +
+    "&eventType=live&type=video&key=" +
+    apiKey;
+
+  axios
+    .get(queryString)
+    .then((response) => {
+      if (response.status.valueOf() !== 403) {
+        console.log("[YT-API]: Received the following from Youtube API: ");
+        console.log(response.data);
+
+        let youtube = response.data;
+
+        if (youtube.items !== null) {
+          if (youtube.items.length > 0) {
+            videoId = youtube.items[0].id.videoId;
+            console.log("[YT-API]: New videoId: " + videoId);
+            updateYoutubeStream(videoId);
+          } else {
+            console.error("[YT-API]: No new items to update.");
+            updateYoutubeStream("XdMBqIeeADA");
+          }
+        } else {
+          console.error("[YT-API]: Error reading items from response data.");
+        }
+      } else {
+        console.error("[YT-API]: Quota reached for Youtube API.");
+      }
+    })
+    .catch((e) => {
+      console.error("[YT-API]: API Refresh Error: " + e.message);
+    });
+}
+
+module.exports = {
+  syncClient,
+  getYoutubeStreamFromDatabase,
+  updateYoutubeStream,
+  updateVideoIdFromAPI,
+};
