1f1df71bcd757a1594a239c403ad021ca02c2b93
Desmond <desmondlai30@gmail.com>
2023-05-02 15:06:03 -0400

    Updated dependecies. Added new section to pub.js

--- pages/pub.js
+++ pages/pub.js
@@ -1,46 +1,168 @@
 import Head from "next/head";
-import Footer from "../components/Footer"
-import Link from "next/link"
+import Footer from "../components/Footer";
+import Link from "next/link";
 
 export default function Publications() {
-    return (
-        <div>
-            <Head>
-                <title>UR Newman - Publications</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="Catholic Community at the University of Rochester" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>UR Newman - Publications</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Catholic Community at the University of Rochester"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <div className="newman-primary-color newman-primary-text-color py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12 text-center">
-                            <h1 className="display-5 pb-5">Publications</h1>
-                            <div className="pb-5">
-                                <h3>Annual Reports</h3>
-                                <p><Link href="https://drive.google.com/open?id=1GLu8eYLcNdPyiFFawqZxXUnXklHpuPOI" passHref>2021-2022</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1fkyFdDmnC_D-4LcslBS5aRTwI3Dju6_W" passHref>2020-2021</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1GfNu-sRgn3Fdu6V5EUPsFFq__yoaT36J" passHref>2019-2020</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1AuWAobwK61gdXUArvstIdYcz-JTXvJPm" passHref>2018-2019</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1Tq9rbO022azQ3qCT137bJLqeVb7BQv1B" passHref>2017-2018</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=0B_146wWmYBsVUmtWRUluMHUxcjg" passHref>2016-2017</Link></p>
-                            </div>
-                            <div>
-                                <h3>Newsletter</h3>
-                                <p><Link href="https://drive.google.com/open?id=1FK2cMLsjcBDFBk4LuFbd53owLDnB5FDx" passHref>Spring 2022</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1K6_5RG1AsTuuvrPJjHlDfhB6B86ozLjY" passHref>Fall 2021</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1Ld9_Nn74kPZ5ulZWLt4lc6_v5BTYF6hO" passHref>Spring 2021</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=1xaNkK0YVQ0tEg299V-_3vpxPCGG1bjne" passHref>Fall 2020</Link></p>
-                                <p><Link href="https://drive.google.com/open?id=17xx3kynVjV2ms8bpaYTomdOZxIjNo-LG" passHref>Spring 2020</Link></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <div className="newman-primary-color newman-primary-text-color py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h1 className="display-5 pb-5">Publications</h1>
+              <div className="pb-5">
+                <h3>Creating a Safe Environment</h3>
+                <p>
+                  <Link
+                    href="https://www.dor.org/safe-environment/office-of-safe-environment/"
+                    passHref
+                  >
+                    DOR Website
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://www.dor.org/wp-content/uploads/SafeEnvironment_Winter_2023.pdf"
+                    passHref
+                  >
+                    Winter 2023
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://www.dor.org/wp-content/uploads/SafeEnvironment_Fall_2022.pdf"
+                    passHref
+                  >
+                    Fall 2022
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://www.dor.org/wp-content/uploads/SafeEnvironment_Summer_2022.pdf"
+                    passHref
+                  >
+                    Summer 2022
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://www.dor.org/wp-content/uploads/SafeEnvironment_Spring_2022.pdf"
+                    passHref
+                  >
+                    Spring 2022
+                  </Link>
+                </p>
+              </div>
+              <div className="pb-5">
+                <h3>Annual Reports</h3>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1GLu8eYLcNdPyiFFawqZxXUnXklHpuPOI"
+                    passHref
+                  >
+                    2021-2022
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1fkyFdDmnC_D-4LcslBS5aRTwI3Dju6_W"
+                    passHref
+                  >
+                    2020-2021
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1GfNu-sRgn3Fdu6V5EUPsFFq__yoaT36J"
+                    passHref
+                  >
+                    2019-2020
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1AuWAobwK61gdXUArvstIdYcz-JTXvJPm"
+                    passHref
+                  >
+                    2018-2019
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1Tq9rbO022azQ3qCT137bJLqeVb7BQv1B"
+                    passHref
+                  >
+                    2017-2018
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=0B_146wWmYBsVUmtWRUluMHUxcjg"
+                    passHref
+                  >
+                    2016-2017
+                  </Link>
+                </p>
+              </div>
+              <div>
+                <h3>Newsletter</h3>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1FK2cMLsjcBDFBk4LuFbd53owLDnB5FDx"
+                    passHref
+                  >
+                    Spring 2022
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1K6_5RG1AsTuuvrPJjHlDfhB6B86ozLjY"
+                    passHref
+                  >
+                    Fall 2021
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1Ld9_Nn74kPZ5ulZWLt4lc6_v5BTYF6hO"
+                    passHref
+                  >
+                    Spring 2021
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=1xaNkK0YVQ0tEg299V-_3vpxPCGG1bjne"
+                    passHref
+                  >
+                    Fall 2020
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    href="https://drive.google.com/open?id=17xx3kynVjV2ms8bpaYTomdOZxIjNo-LG"
+                    passHref
+                  >
+                    Spring 2020
+                  </Link>
+                </p>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
 
-
-            {/*<div className="subpage-cards pb-5">
+      {/*<div className="subpage-cards pb-5">
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12 text-center">
@@ -56,7 +178,7 @@ export default function Publications() {
                 </div>
             </div>*/}
 
-            <Footer />
-        </div>
-    )
+      <Footer />
+    </div>
+  );
 }
